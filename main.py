51116520f74b9d063ef24bb66e2857c12280e1bf
emorosanu <edoardo.morosanu@ittvt.edu.it>
2023-07-27 22:12:07 +0200

    added more file extensions

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import os
 
 p = os.path.expanduser('~') + '/Downloads'
-dirs = ['Images', 'Documents', 'Videos', 'Books', 'Music', 'Compressed']
+dirs = ['Images', 'Documents', 'Videos', 'Books', 'Music', 'Compressed', 'Executables']
 extensions = [
     ["jpg", "jpeg", "jpe", "jif", "jfif", "jfi", "png", "gif", "webp", "tiff", "tif", "psd", "raw", "arw", "cr2", "nrw",
      "k25", "bmp", "dib", "heif", "heic", "ind", "indd", "indt", "jp2", "j2k", "jpf", "jpf", "jpx", "jpm", "mj2", "svg", "svgz", "ai", "eps", "ico"],
-    ["doc", "docx", "odt", "pdf", "xls", "xlsx", "ppt", "pptx"],
+    ["doc", "docx", "odt", "pdf", "xls", "xlsx", "ppt", "pptx", "csv", "json"],
     ["webm", "mpg", "mp2", "mpeg", "mpe", "mpv", "ogg","mp4", "mp4v", "m4v", "avi", "wmv", "mov", "qt", "flv", "swf", "avchd"],
     ['epub'],
-    ['mp3'],
-    ['zip']
+    ['mp3', 'wav', 'flac', 'm4a', 'aac', 'ogg'],
+    ['zip', 'rar', '7z', 'tar.gz', 'gz', 'tgz', 'tar.xz', 'xz', 'tar.bz2', 'bz2', 'tbz2', 'tar.lzma', 'tlz', 'lzma', 'txz', 'zipx', 'tar', 'iso'],
+    ['exe', 'msi', 'AppImage', 'tar', 'deb', 'rpm', 'apk', 'dmg', 'pkg', 'jar'],
 ]
 
 
