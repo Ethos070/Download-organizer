e0377a4fd0f9c5141f1b333f9b5f637d23a7eb00
ArchSucks <aledionisi05@gmail.com>
2023-07-27 21:56:20 +0200

    Yes

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import os
 p = os.path.expanduser('~') + '/Downloads'
 dirs = ['Images', 'Documents', 'Videos', 'Books', 'Music', 'Compressed']
 extensions = [
-    ['png', 'jpeg', 'jpg'],
-    ['pdf', 'xlsx', 'csv'],
-    ['mp4'],
+    ["jpg", "jpeg", "jpe", "jif", "jfif", "jfi", "png", "gif", "webp", "tiff", "tif", "psd", "raw", "arw", "cr2", "nrw",
+     "k25", "bmp", "dib", "heif", "heic", "ind", "indd", "indt", "jp2", "j2k", "jpf", "jpf", "jpx", "jpm", "mj2", "svg", "svgz", "ai", "eps", "ico"],
+    ["doc", "docx", "odt", "pdf", "xls", "xlsx", "ppt", "pptx"],
+    ["webm", "mpg", "mp2", "mpeg", "mpe", "mpv", "ogg","mp4", "mp4v", "m4v", "avi", "wmv", "mov", "qt", "flv", "swf", "avchd"],
     ['epub'],
     ['mp3'],
     ['zip']
