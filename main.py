11e7e858aa9529326ddb3b4594b598becd6bbbfe
ArchSucks <aledionisi05@gmail.com>
2023-07-27 18:25:35 +0200

    Creates folders and then moves files into them

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import os
 
 p = '/home/alessandro/Downloads'
+dirs = ['Images', 'Documents', 'Videos', 'Books', 'Music', 'Compressed']
+extensions = [
+    ['png', 'jpeg', 'jpg'],
+    ['pdf', 'xlsx', 'csv'],
+    ['mp4'],
+    ['epub'],
+    ['mp3'],
+    ['zip']
+]
 
-if 'Images' not in os.listdir(p):
-    os.mkdir(p + '/Images')
+for d in dirs:
+    if d not in os.listdir(p):
+        os.mkdir(f'{p}/{d}')
 
-for file in os.listdir(p):
-    if '.png' in file:
-        os.rename(f'{p}/{file}', f'{p}/Images/{file}')
+while True:
+    for file in os.listdir(p):
+        for file_type in extensions:
+            for ext in file_type:
+                if f'.{ext}' in file:
+                    ind = extensions.index(file_type)
+                    folder = dirs[ind]
+                    os.rename(f'{p}/{file}', f'{p}/{folder}/{file}')
